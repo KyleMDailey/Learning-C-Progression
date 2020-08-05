4f7c7cdf72e1adaa3a1035b91b9c299e18c0ad93
KyleMDailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 15:08:43 -0400

    Add files via upload

---
+++ Average3Numbers_DaileyKyle07152020/Average3Numbers_DaileyKyle07152020/Program.cs
@@ -0,0 +1,39 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/15/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application that finds the average of 3 numbers.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace Average3Numbers_DaileyKyle07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, average;
+
+            Console.Write("Enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            average = (number1 + number2 + number3) / 3;
+
+            Console.Write("Average of three numbers is {0}", average);
+
+            Console.ReadKey();
+        }
+    }
+}
