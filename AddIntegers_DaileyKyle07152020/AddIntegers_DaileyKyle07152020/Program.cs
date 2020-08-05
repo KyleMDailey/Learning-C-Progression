14b814cc97879ca57fa0f68c880dc02f522d0c1a
KyleMDailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 14:19:19 -0400

    Add files via upload

---
+++ AddIntegers_DaileyKyle07152020/AddIntegers_DaileyKyle07152020/Program.cs
@@ -0,0 +1,35 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/15/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application that adds two integers.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace AddIntegers_DaileyKyle07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.WriteLine("Calculate the sum of two numbers: ");
+            Console.Write("Input number 1: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input number 2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.Write("Result: " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
