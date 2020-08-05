5e73aa590df79b2c4000911a143411a10798f7a5
Kyle Dailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 16:52:01 -0400

    Add files via upload

---
+++ RectangleArea_DaileyKyle07222020/RectangleArea_DaileyKyle07222020/Program.cs
@@ -0,0 +1,32 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/22/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application which asks user's the dimensions for a rectangle and calculates the area and displays it to the console.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace RectangleArea_DaileyKyle07222020
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter the length of your triangle: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the width of your triangle: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = length * width;
+            Console.WriteLine("The area of the rectangle is {0}", area);
+        }
+    }
+}
