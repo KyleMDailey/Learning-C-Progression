8c26a635bf96a54127105b7c165244de7b257f80
KyleMDailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 14:45:00 -0400

    Add files via upload

---
+++ AreaPerimeterCircle_DaileyKyle07152020/AreaPerimeterCircle_DaileyKyle07152020/Program.cs
@@ -0,0 +1,34 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/15/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application that computes the area and perimeter of a circle.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace AreaPerimeterCircle_DaileyKyle07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, perimeter, area;
+            Console.Write("Please enter the radius of your circle: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            perimeter = 2 * 3.14 * r;
+            area = 3.14 * Math.Pow(r, 2);
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("The perimeter of your circle: {0}", perimeter);
+            Console.WriteLine("The area of your circle: {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
