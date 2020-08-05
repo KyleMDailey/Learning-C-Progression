dda46f676aa88f3cdd0d41a7061cc7c57fad92b8
Kyle Dailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 15:34:04 -0400

    Add files via upload

---
+++ ComputeQuotientRemainder_DaileyKyle07152020/ComputeQuotientRemainder_DaileyKyle07152020/Program.cs
@@ -0,0 +1,34 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/15/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application that computes quotients and remainders.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace ComputeQuotientRemainder_DaileyKyle07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend: {0} Divisor {1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
