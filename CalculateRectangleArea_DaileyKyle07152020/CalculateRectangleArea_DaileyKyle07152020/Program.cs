e8b1b22048b1c036390e7194bb745f7c2f459980
KyleMDailey <67755613+KyleMDailey@users.noreply.github.com>
2020-08-05 15:18:05 -0400

    Add files via upload

---
+++ CalculateRectangleArea_DaileyKyle07152020/CalculateRectangleArea_DaileyKyle07152020/Program.cs
@@ -0,0 +1,33 @@
+﻿//Student:  Kyle Dailey
+//Date:  07/15/2020
+
+/**************************************************************************************************************************************************************************************
+This project contains code for a console application that computes the area of a rectangle.  
+
+Revisions:  None
+
+Revision Date:  N/A
+
+Revised By:  N/A
+
+***************************************************************************************************************************************************************************************/
+
+using System;
+
+namespace CalculateRectangleArea_DaileyKyle07152020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int area, length, width;
+            Console.Write("Please enter the length of your rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the width of your rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            area = length * width;
+            Console.WriteLine("The area of the rectangle: {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
